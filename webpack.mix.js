17cffe2ba2b57cb01b3ace520ab4dd487264dbf3
EvenhuisD <devenhuis81@gmail.com>
2021-04-07 20:13:56 +0200

    tymon jwt-auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -4,4 +4,4 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [])
     .webpackConfig(webpack => { return {
         plugins: [new webpack.DefinePlugin({ '__VUE_OPTIONS_API__': 'true', '__VUE_PROD_DEVTOOLS__': 'false' })],
-    }}).vue()
+    }}).vue().sourceMaps()

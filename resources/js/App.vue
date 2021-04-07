366a1b7742c39c5ad08dc0ffc2286f903b7b4701
EvenhuisD <devenhuis81@gmail.com>
2021-04-07 19:32:16 +0200

    store, router,  css

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,3 @@
 <template>
-    <div>App</div>
+    <router-view />
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import store from './store'
+import router from './router'
 
 const app = createApp(App)
+app.use(store)
+app.use(router)
+
+const files = require.context('./components', true, /\.vue$/i)
+files.keys().map(key => app.component(key.split('/').pop().split('.')[0], files(key).default))
+
 app.mount('#app')

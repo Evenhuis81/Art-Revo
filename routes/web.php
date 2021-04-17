1304885622999ab153157c40e7978e518ac51fd6
EvenhuisD <devenhuis81@gmail.com>
2021-04-17 16:31:19 +0200

    testmail

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MailController;
+
+Route::get('/send-email', [MailController::class, 'sendEmail']);
 
 Route::get('/{any}', fn () => view('app'))->where('any', '.*');
